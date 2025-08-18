044af2f450acafdd7f4910d329f12610e06741a7
saviour623 <saviourme33@gmail.com>
2025-08-18 14:21:26 +0100

    update

--- test/float.c
+++ test/float.c
@@ -11,6 +11,8 @@
 #define putd(d)  printf("%lld\n", (long long)(d))
 #define puthx(x) printf("%#llx\n", (long long)(x))
 #define putf(f)  printf("%.20f\n", (double)(f))
+#define PASS(...) (void *)0
+#define expOf(...) true
 
 #if defined(__GNUC__) || defined(__clang__)
     #define FORCEI_PURE __attribute__((pure, always_inline))
@@ -20,9 +22,6 @@
     #define FORCEI_PURE
 #endif
 #define INLINE(T) __inline__ FORCEI_PURE T
-
-#define PASS(...) (void *)0
-#define expOf(...) true
 
 #if defined(__GNUC__) || defined(__clang__) || ((-16 >> 1) == -8 && ((-1 & (16 - 1)) == 15))
 // Check if compiler supports correct bit operations on signed bits (according to the standard, this is implementation defined)
@@ -39,6 +38,7 @@
 #endif
 
 // We require an integer of bit-width W, wide enough to accomodate the underlying bit-width of float or double
+
 #if !defined(UINT64_MAX) || defined(USE_FLOAT32__)
     #define DISABLE_DBL_SUPPORT__
     #ifndef UINT32_MAX
@@ -89,31 +89,31 @@ DBLT__ exp__       (const DBLT__ x);
 static const DBLT__ Precalc_2powDdivP[16] =
   {
    // 2^d/p or p_root(2^d); P = 16
-   1.00000000000000000, 1.04427378242741384, 1.09050773266525766,
-   1.13878863475669165, 1.18920711500272107, 1.24185781207348405,
-   1.29683955465100967, 1.35425554693689273, 1.41421356237309505,
-   1.47682614593949931, 1.54221082540794079, 1.61049033194925431,
-   1.68179283050742909, 1.75625216037329948, 1.83400808640934246,
-   1.91520656139714729
+   1.00000000000000000, 1.0442737824274138, 1.0905077326652577,
+   1.13878863475669160, 1.1892071150027210, 1.2418578120734840,
+   1.29683955465100960, 1.3542555469368927, 1.4142135623730951,
+   1.47682614593949930, 1.5422108254079407, 1.6104903319492543,
+   1.68179283050742900, 1.7562521603732995, 1.8340080864093424,
+   1.91520656139714740
   };
 
-#define Precalc_Log10b2  3.32192809488736235
-#define Precalc_Log2b10  0.30102999566398120
-#define Precalc_Loge2    0.69314718055994531
-#define Precalc_InvLoge2 1.44269504088896341
-#define Precalc_Loge10   2.30258509299404684
+#define Precalc_Log10b2  3.3219280948873620
+#define Precalc_Log2b10  0.3010299956639812
+#define Precalc_Loge2    0.6931471805599453
+#define Precalc_InvLoge2 1.4426950408889630
+#define Precalc_Loge10   2.3025850929940460
 
 #define Precalc_1_f_asInt    0x3ff0000000000000ull
 #define Precalc_FloatExpBias 0x10000000000000ull
 #define Precalc_53shift52    0x350000000000000ull
 
-#define Precalc_SQRT2              1.414213562373095049
-#define Precalc_LogSQRT2b10        0.150514997831990598
-#define Precalc_DydfLogSQRT2_b10   0.307092573185687714
-#define Precalc_D2ydf2LogSQRT2_b10_Div2 0.10857362047581296
+#define Precalc_SQRT2              1.4142135623730951
+#define Precalc_LogSQRT2b10        0.1505149978319906
+#define Precalc_DydfLogSQRT2_b10   0.3070925731856877
+#define Precalc_D2ydf2LogSQRT2_b10_Div2 0.108573620475813
 
-#define Precalc_16DivLoge2 23.08312065422341452
-#define Precalc_Loge2Div16 4.332169878499658e-2
+#define Precalc_16DivLoge2 23.083120654223414
+#define Precalc_Loge2Div16 4.3321698784997e-2
 
 #define approxLog2p(x) (0.5 * approxLog2(x) + approxLog2p(x*0.66667) * 0.6667)
 
@@ -220,12 +220,13 @@ extern INLINE(DBLT__) exp__(const DBLT__ x) {
 
    if (LIKELY___((Q < 0), 0)) {
      p2.F = Precalc_2powDdivP[16 - (-Q & 0xf)];  // -N mod p == 16 - (N mod 16)  // N mod 16 =~ N & (16 - 1)
-     p2.N -= (UINT_)((-Q >> 4) + 1) << DBLT_MANT_SHFT;
+     p2.N -= ((-Q >> 4) + 1) << DBLT_MANT_SHFT;
 
      goto exp;
    }
 #endif
    // Q >= 0 | bit operations are guaranteed to be valid on signed negative integers
+   puti(Q & 0xf);
    p2.F = Precalc_2powDdivP[Q & 0xf];
    p2.N += (Q >> 4) << DBLT_MANT_SHFT; // Safe, since Q>>4 is always small (< 5)
 
@@ -236,58 +237,58 @@ extern INLINE(DBLT__) exp__(const DBLT__ x) {
 }
 extern INLINE(DBLT__) exp___(const DBLT__ x) {
   /* By continued fraction,
-     e^x = 1 + 2x / (2 - x + x^2 / (6 + x^2 / (10 + x^2 / 14 + ...
-     =~ 1 + 2x / [[(2 - x)(840 + 20x^2) + (140 + x^2)x^2] / 840 + 20x^2]  (5 terms only)
-     * Let A = 840 + 20x^2
+     e^x = 1 + 2x / (2 - x + x^2 / (6 + x^2 / (10 + x^2 / 14 + ...)))
+     =~ 1 + 2x(840 + 20x^2) / [(2 - x)(840 + 20x^2) + (140 + x^2)x^2]  (5 terms only)
 */
-  const DBLT__ x2 = x * x, a = 840. + 20. * x2;
+  const DBLT__ x2 = x * x, a = 840. + 20. * x2; //Let A = 840 + 20x^2
 
-  // exp(x) =~ 1 + 2x * A * [(2 - x)*A + x^2(140 + x^2)]  (simplified fraction)
+  // exp(x) =~ 1 + 2x * A * [(2 - x)*A + x^2(140 + x^2)]  (simplified the C.F)
   return 1. + 2. * x * a / ((2. - x) * a + x2 * (140. + x2));
 }
 
-#if (MAX_PREC_CALC_FLT)
-    #define REDUCE_TO0(f) f * 0.1
-    #define CORRECT_REDC(f) xpow10__(f)
-#else
-    #define REDUCE_TO0
-    #define CORRECT_REDC
-#endif
-
-extern inline DBLT__ reprexp(bits_t bits) {
-  int16_t p2, p10;
+extern INLINE(DBLT__) reprexp(bits_t bits) {
+  int16_t p2, p10, p2tmp;
   DBLT__  F,  dF,  dp, dp2;
 
-  F = bits.F; //keep value
+  F = bits.F; // Float value
 
   //  Exponent
-  p2 = (bits.N >> DBLT_MANT_SHFT) - DFLT_BIAS__;
+  p2tmp = p2 = (bits.N >> DBLT_MANT_SHFT) - DFLT_BIAS__;
+  puti(p2);
 
   // Normalized Mantissa. Within [1, 2)
   bits.N = (bits.N & Precalc_AllOnesMantBits) | (DFLT_BIAS__ << DBLT_MANT_SHFT);
 
-  // The exponent of F is Approximated using log10(f) where f ~ log(2^p * M)
-  // ~ log(2^p * M) ~ log(2^p * M) ~ plog(2) + log(M)
-  // And log(M) == ln(M) / ln(10):
-  // The contribution log(M) to the exponent is approximated using taylor’s series of Log(M) about point b (where b == SQRT(2))
+  /* The exponent of F is Approximated using log10(f) where f ~ 2^p * M
+   * log(F) ~ log(2^p * M) ~ plog(2) + log(M)
+   * And log(M) == ln(M) / ln(10):
+   * The contribution log(M) to the exponent is approximated using taylor’s series of Log(M) about point b (where b == SQRT(2))
 
-  // Let y = Ln(f)
-  // The Taylor approx. is given as Ln(b) + dF*dy/df - dF^2*(1/2*d2y/df2)...
-  dF = bits.F - Precalc_SQRT2; // f - a
+   * Let y = log(f)
+   * The Taylor approx. is given as log(b) + dF*dy/df - dF^2*(1/2*d2y/df2)...
+   */
+   dF = bits.F - Precalc_SQRT2; // f - a
 
-  p10 =(p2 * Precalc_Log2b10) + (Precalc_LogSQRT2b10 + ((dF * Precalc_DydfLogSQRT2_b10) - (dF * (dF * Precalc_D2ydf2LogSQRT2_b10_Div2))));
+  p10 = (p2 * Precalc_Log2b10) + (Precalc_LogSQRT2b10 + ((dF * Precalc_DydfLogSQRT2_b10) - (dF * (dF * Precalc_D2ydf2LogSQRT2_b10_Div2))));
 
-  // Reduce F to [1, 2)
-  // That is, F has to be multiplied by 10^-p such that F  1 and == f and f x 10^p == F
-  p2  = (Precalc_Log10b2 * p10); // log2(p10)
-  // Split 10^p into 2^-p2 * 10^dp [dp = 10 ^ (p10 - p2)]
-  // 10 ^ dp is approximated using e^Ln(dp)
-  dp  = (-p10 * Precalc_Loge10) + (p2 * Precalc_Loge2);
+  /* Reduce F such that floor(abs(F)) < 10
+   * That is, We divide F by 10^p (F ~ N1N2N3... becomes F ~ N1.N2N3...)
+   *
+   * 10^p is spltted into 2^p2 * 10^dp (where: 2^p2 <= 10^p < 2^(p2+1))
+   *
+   * p2 = trunc( log2(p) )
+   * dp = p - p2
+   */
+  bits.F = F;
+  p2  = (Precalc_Log10b2 * p10); // p2 = log2(p)
+  bits.N = (bits.N & Precalc_AllOnesMantBits) | ((DFLT_BIAS__ - p2 + ptmp) << DBLT_MANT_SHFT); // F / 2^p2
 
-  bits.N = (DFLT_BIAS__ - p2) << DBLT_MANT_SHFT; // 2^p2
+  // We take the natural logarithmic difference, so that 10^dp ~ e^Ln(dp)
+  // making it an ease to calculate, since dp is small and e^dp has good and cheap approximation techniques for small numbers (< 1)
+  dp  = (p10 * Precalc_Loge10) - (p2 * Precalc_Loge2); // p - p2
   dp = exp__(dp); // e^Ln(dp) ~ 10^(p10 - p2)
 
-  return 5e-14 + F * (dp * bits.F); // F * 10^p or  F * (2^p2 * 10^dp)
+  return 5e-14 + (bits.F / dp); // (F / 2^p) / 10^dp
 }
 
 static inline void floatRepr(DBLT__ f) {
@@ -310,7 +311,7 @@ static inline void floatRepr(DBLT__ f) {
 
 int main(void) {
   bits_t k;
-  k.F = 9802395802.;
+  k.F = 2342344534.;
 
   putf(reprexp(k));
   putf(k.F);
